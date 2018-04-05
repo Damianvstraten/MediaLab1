f753f4e4659f3b7ed2858823f28c1610d63b098c
Damian van Straten <damianvanstraten@hotmail.com>
2018-04-05 15:37:44 +0200

    save multiple images

--- index.php
+++ index.php
@@ -1,31 +1,40 @@
 <?php
  include "layout/header.php";
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["images"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+function reArrayFiles(&$file_post) {
 
-    $check = getimagesize($_FILES["images"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
+    $file_ary = array();
+    $file_count = count($file_post['name']);
+    $file_keys = array_keys($file_post);
+
+    for ($i=0; $i<$file_count; $i++) {
+        foreach ($file_keys as $key) {
+            $file_ary[$i][$key] = $file_post[$key][$i];
+        }
     }
 
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
-    } else {
-        if (move_uploaded_file($_FILES["images"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["images"]["name"]). " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
+    return $file_ary;
+}
+
+
+if(isset($_POST["submit"])) {
+    $target_dir = "uploads/";
+
+    if (!file_exists($target_dir)) {
+        mkdir('uploads/', 0777, true);
+    }
+
+    if ($_FILES['images']) {
+        $file_ary = reArrayFiles($_FILES['images']);
+
+        foreach ($file_ary as $file) {
+            echo $target_file = $target_dir . basename($file['name']);
+
+            if (move_uploaded_file($file['tmp_name'], $target_file)) {
+                echo "The file " . basename($file['name']) . " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
         }
     }
 }
@@ -49,7 +58,7 @@ if(isset($_POST["submit"])) {
                         <img src="images/plus-symbol-in-a-rounded-black-square.png">
                     </label>
 
-                    <input id="file-input" name="images" type="file" accept="image/*" multiple/>
+                    <input id="file-input" name="images[]" type="file" accept="image/*" multiple />
                 </div>
                 <input type="submit" name="submit">
             </form>
