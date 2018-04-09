11cd2a1097001215bc4689d4513d081d2b76e1cb
Soloshadow <0888150@hr.nl>
2018-04-09 14:23:51 +0200

    added some functionality to share.php

--- js/app.js
+++
@@ -1,9 +0,0 @@
-let checkbox = document.getElementById("myonoffswitch4");
-
-checkbox.addEventListener("click", () => {
-    if(checkbox.checked == true){
-        console.log("this is true");
-    }else{
-        console.log("this is false");
-    }
-})
